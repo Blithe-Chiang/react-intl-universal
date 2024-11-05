6245bf087390b142263be01362eadb44164b97d5
jiangzs <2373806028@qq.com>
2024-11-05 10:15:34 +0800

    demo改造

--- demo/notali/src/App.js
+++ demo/notali/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>{intl.get('1920159459').d('这里是测试汉子')}</div>
-      <div>{intl.get('125442853').d('这里是测试汉子${e}你好')}</div>
+      <div>{intl.get("1920159459").d("这里是测试汉子")}</div>
+      <div>{intl.get("125442853").d("这里是测试汉子2{e}你好")}</div>
       <div>{"这是一个不存在的文字"}</div>
     </div>
   );
